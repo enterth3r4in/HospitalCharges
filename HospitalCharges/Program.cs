4d4cfde45ef4f83b3cad9e3420308ae16315c683
enterth3r4in <believe.the.rain@gmail.com>
2016-03-28 12:37:40 -0400

    Commenting

--- HospitalCharges/Program.cs
+++ HospitalCharges/Program.cs
@@ -6,15 +6,16 @@ using System.Windows.Forms;
 
 
 /*
- * Project Name: 
- * Project Purpose:
- * Input:
- * Desired Output:
- * Variables:
- * Classes:
- * Formulas:
- * Main Algorithm:
- * Date: 
+ * Project Name: Hospital Charges
+ * Project Purpose: To calculate and display a hospital bill
+ * Input: User inputs in text boxes and button clicks
+ * Desired Output: Should display a form with properly calculated bill
+ * Variables: doubles store inputs
+ * Classes: Extra form to display bill cleanly
+ * Formulas: Calculations are performed in separate methods, strings concatenated
+ * Main Algorithm: Lets user input charges and click buttons, then when they click calculate
+ * it writes up the bill and displays it in a separate form which can be closed separately.
+ * Date: 3-24-2016
  */
 namespace HospitalCharges
 {
